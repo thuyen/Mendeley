0687028504c4e75366f71c140cd1bf32edf1799d
thuyenvn <vanthuyen@gmail.com>
2014-02-24 22:50:51 -0800

    Readme updated

--- mdl.sh
+++ mdl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-p -uv "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup5" "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup6"
+cp -uv "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup5" "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup6"
 cp -uv "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup4" "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup5"
 cp -uv "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup3" "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup4"
 cp -uv "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup2" "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup3"
 cp -uv "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup1" "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup2"
 cp -uv "/home/thuyen/Papers/Mendeley/.your@email.com@www.mendeley.com.sqlite" "/home/thuyen/.local/share/data/Mendeley Ltd./Mendeley Desktop/.your@email.com@www.mendeley.com.sqlite.backup1"
-~/Papers/Mendeley/.MendeleyFileOrganizer.py ~/Papers/.your@email.com@www.mendeley.com.sqlite ~/Papers
+~/Papers/Mendeley/MendeleyFileOrganizer.py ~/Papers/.your@email.com@www.mendeley.com.sqlite ~/Papers
